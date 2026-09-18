c238de67b1f944369de33499afba3b4359ee95df
ahmadfahriyanhafi <ahmadfahriyanhafi2712@gmail.com>
2026-09-18 23:42:43 +0800

    Add files via upload

---
+++ Ucapan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Salam;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Ucapan {
+    public static void main(String[] args){
+        System.out.println("Halo ULM");
+        System.out.println("ULM berdaya saing dan unggul di bidang lahan basah");
+        System.out.println("Prodi ilkom unggul dengan teknologi kekinian");
+    }
+}
