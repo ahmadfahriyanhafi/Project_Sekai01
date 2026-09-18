4a709248b346a8324b157c2ced960ee06b288efe
ahmadfahriyanhafi <ahmadfahriyanhafi2712@gmail.com>
2026-09-18 23:44:20 +0800

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author Administrator
+ */
+public class LuasSegitiga {
+    public static void main(String[] args) {
+        int tg = 3;
+        double alas = tg * 5;
+        double luas = alas * 0.5;
+        System.out.println(luas);
+    }
+}
